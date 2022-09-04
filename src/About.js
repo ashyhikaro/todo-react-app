ffca10feba182b24e4629d61992d47a2070d744a
Oleh Marynchuk <happyhikaro@gmail.com>
2022-09-04 14:17:49 +0300

    some change in text

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
       the React framework. You can use it for your own purposes, 
       the code is completely open, all you need is to download the files, and enter 
       the command <strong>npm install</strong> in the terminal
-      Thank you!
+      Thank you!!
     </p>
     </>
   );
